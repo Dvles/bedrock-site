d480a8c77ff0449687a82330c1f7177755b93b0e
Dvles <132080646+Dvles@users.noreply.github.com>
2025-05-07 23:05:24 +0200

    enqueue styles, fonts and scripts

---
+++ web/app/themes/topiclisting/functions.php
@@ -0,0 +1,98 @@
+<?php
+
+
+
+// Enqueues style.css on the front.
+if (! function_exists("topiclisting_enque_styles")){
+
+    function topiclisting_enque_styles(){
+        $theme_uri = get_template_directory_uri();
+
+        wp_enqueue_style(
+            'topiclisting-bootstrap',
+            "$theme_uri/assets/css/bootstrap.min.css",
+            [],
+            '1.0',
+            'all'
+        );
+
+        wp_enqueue_style(
+            'topiclisting-bootstrap-icons',
+            "$theme_uri/assets/css/bootstrap-icons.css",
+            ['topiclisting-bootstrap'], 
+            '1.0',
+            'all'
+        );
+
+        wp_enqueue_style(
+            'topiclisting-css',
+            "$theme_uri/assets/css/templatemo-topic-listing.css",
+            ['topiclisting-bootstrap', 'topiclisting-bootstrap-icons'],
+            '1.0',
+            'all'
+        );
+    }
+}
+
+// Enqueues scripts on the front
+if (! function_exists("topiclisting_enque_scripts")){
+
+    function topiclisting_enque_scripts(){
+        $theme_uri = get_template_directory_uri();
+
+        wp_enqueue_script("jquery", "$theme_uri/assets/js/jquery.min.js", [], 1.0, 'all');
+
+        wp_enqueue_script(
+            'bootstrap',
+            "$theme_uri/assets/js/bootstrap.bundle.min.js",
+            ['jquery'],
+            '1.0',
+            true
+        );
+
+        wp_enqueue_script(
+            'jquery-sticky',
+            "$theme_uri/assets/js/jquery.sticky.js",
+            ['jquery'],
+            '1.0',
+            true
+        );
+
+        wp_enqueue_script(
+            'click-scroll',
+            "$theme_uri/assets/js/click-scroll.js",
+            ['jquery'],
+            '1.0',
+            true
+        );
+
+        wp_enqueue_script(
+            'custom-js',
+            "$theme_uri/assets/custom.js",
+            ['jquery', 'bootstrap', 'jquery-sticky', 'click-scroll'],
+            '1.0',
+            true
+        );
+    }
+}
+
+
+add_action('wp_enqueue_styles', 'topiclisting_enque_styles');
+add_action('wp_enqueue_scripts', 'topiclisting_enque_scripts');
+
+
+function topiclisting_enqueue_google_fonts() {
+    // Preconnect for performance
+    wp_enqueue_style( 'google-fonts-preconnect', 'https://fonts.googleapis.com', [], null );
+    wp_enqueue_style( 'google-fonts-preconnect2', 'https://fonts.gstatic.com', [], null );
+    wp_style_add_data( 'google-fonts-preconnect2', 'crossorigin', 'anonymous' );
+
+    // font request
+    wp_enqueue_style(
+        'google-fonts',
+        'https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap',
+        [],
+        null
+    );
+}
+add_action( 'wp_enqueue_scripts', 'topiclisting_enqueue_google_fonts' );
