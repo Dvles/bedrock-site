acc2dbd3774dc00b6fc4c0f18e70867f22095861
Dvles <132080646+Dvles@users.noreply.github.com>
2025-05-08 11:28:41 +0200

    feat(theme): split HTML into header.php and footer.php for WordPress templating

---
+++ web/app/themes/topiclisting/header.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title><?php bloginfo('name'); ?></title>
+    
+    <?php wp_head(); ?>
+</head>
