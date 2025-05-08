0d0f50c6db061ae8e937b15cf39d3928413031b2
Dvles <132080646+Dvles@users.noreply.github.com>
2025-05-08 15:58:12 +0200

    feat: make homepage feature blocks editable via ACF fields
    - Added ACF fields for small and large feature content blocks
    - Enabled dynamic titles, text, images, badges, and links

---
+++ web/app/themes/topiclisting/front-page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+    <main>
+
+    <?php get_template_part('template-parts/navigation'); ?>
+    <?php get_template_part('template-parts/hero'); ?>
+    <?php get_template_part('template-parts/featured'); ?>
+        
+    </main>
+
+<?php get_footer(); ?>
+
+
+
+
