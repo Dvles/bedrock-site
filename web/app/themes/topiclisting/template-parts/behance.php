6e899163f7c997026e44ef810198fd97de92eea1
Dvles <132080646+Dvles@users.noreply.github.com>
2025-05-11 12:20:48 +0200

    Enhance projects styling to match template

--- web/app/themes/topiclisting/template-parts/behance.php
+++ web/app/themes/topiclisting/template-parts/behance.php
@@ -26,7 +26,7 @@ $projects = $xpath->query('//div[contains(@class, "beh-projects")]');
     </div>
 
     <div class="col-12">
-      <div class="splide" id="splide-latest-projects">
+      <div class="splide py-4 px-2" id="splide-latest-projects">
         <div class="splide__track">
           <ul class="splide__list">
             <?php foreach ($projects as $project):
@@ -37,20 +37,11 @@ $projects = $xpath->query('//div[contains(@class, "beh-projects")]');
               if (empty($img)) {
                 $img = $imgEl->getAttribute('src');
               }
-
-              $title = html_entity_decode($imgEl->getAttribute('alt'), ENT_QUOTES | ENT_HTML5, 'UTF-8');
             ?>
               <li class="splide__slide">
-                <div class="custom-block bg-white shadow-lg rounded-xl">
-                  <a href="<?= esc_url($link) ?>" class="d-block" target="_blank" rel="noopener">
-                    <div class="p-3">
-                      <h5 class="mb-2"><?= $title ?></h5>
-                      <p class="mb-0">Behance Projects</p>
-                    </div>
-                    <img src="<?= esc_url($img) ?>" class="custom-block-image img-fluid" alt="<?= esc_attr($title) ?>">
-
-                  </a>
-                </div>
+                <a href="<?= esc_url($link) ?>" class="d-block" target="_blank" rel="noopener">
+                  <img src="<?= esc_url($img) ?>" class="custom-block-image  behance-slide-image img-fluid rounded" alt="<?= esc_attr($title) ?>">
+                </a>
               </li>
             <?php endforeach; ?>
           </ul>
